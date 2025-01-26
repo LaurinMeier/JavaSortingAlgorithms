66a7a7615bf13a76c668eba1e36a803908db2455
Laurin Meier <44509495+LaurinMeier@users.noreply.github.com>
2025-01-26 14:53:03 +0100

    Added Quick Sort
    Simple, yet elegant, non-type-specific implementation of the quick sort.

---
+++ QuickSort.java
@@ -0,0 +1,34 @@
+package SortingProject;
+
+public class QuickSort {
+    public static <T extends Comparable<T>> void sort(T[] a, int start, int end){
+        int i=start-1;
+        int j=start;
+
+        int pivot = end;
+
+        while(j < pivot){
+            if(a[j].compareTo(a[pivot]) < 0){
+                swap(a, ++i, j++);
+            }
+            else{
+                j++;
+            }
+        }
+        swap(a, ++i, pivot);
+
+        if((i-1)-start > 0){
+            sort(a, start, i-1);
+        }
+
+        if(end-(i+1) > 0){
+            sort(a, i+1, end);
+        }
+    }
+
+    private static <T> void swap(T[] a, int b, int c){
+        T temp = a[b];
+        a[b] = a[c];
+        a[c] = temp;
+    }
+}
